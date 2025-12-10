1378476fcb8f242427f6ddad4c3c1759b58baaa7
Onigui <onimushapower@gmail.com>
2025-12-09 23:56:07 -0300

    fix: Adicionar campos de preferências nos selects do backend

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1943,6 +1943,8 @@ export default async function handler(req: any, res: any) {
               phone: true,
               cpf: true,
               avatar: true,
+              notificationsEnabled: true,
+              locationEnabled: true,
               role: true,
             }
           });
